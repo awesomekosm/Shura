645832ba958335993c3b1c9e11b1ddadef9b41dd
Eugene Barnett <destrachan2030@gmail.com>
2020-02-09 18:21:22 -0500

    Fix removing container in run.sh

--- run.sh
+++ run.sh
@@ -6,8 +6,6 @@
 
 docker build --tag local/shura:latest .
 
-docker stop shura
-
-docker rm shura
+docker rm -f shura || true
 
 docker run -d --name shura --env JAVA_OPTS="-Ddiscord.token=$1" local/shura
